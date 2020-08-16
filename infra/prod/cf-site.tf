6a88d62052d46e5deaa9b4ac077c04cc44620ae9
Douglas Naphas <douglasnaphas@gmail.com>
2020-08-15 21:18:27 -0400

    Update interpolation syntax in a dependency
    https://github.com/douglasnaphas/hatgame/issues/9

--- infra/prod/cf-site.tf
+++ infra/prod/cf-site.tf
@@ -1,6 +1,6 @@
 module "s3-cf-site" {
   source  = "douglasnaphas/s3-cf-site/aws"
-  version = "0.1.0"
+  version = "0.3.0"
   # insert the 5 required variables here
   bucket_name = var.bucket_name
   cert_domain = var.cert_domain
