6059502efe3d002d6766905d2a287eb81e561fac
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-10 17:11:16 -0400

    Test that the stack can be instantiated
    gh-6

---
+++ infra/test/app.test.ts
@@ -0,0 +1,15 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { AppStack, AppStackProps } from "../lib/app";
+
+test("Can instantiate stack", () => {
+  const app = new cdk.App();
+  //     // WHEN
+  const stack = new AppStack(app, "MyTestStack");
+  //     // THEN
+  const template = Template.fromStack(stack);
+
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
