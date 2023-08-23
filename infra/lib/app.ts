1f92bd2473b48bcbccdee57cfc3dec72e5932741
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-23 08:34:35 -0400

    Try to deploy exported Figma html from frontend/
    gh-6

--- infra/lib/app.ts
+++ infra/lib/app.ts
@@ -1,11 +1,11 @@
-import * as cdk from "aws-cdk-lib";
+import {Stack, StackProps, CfnOutput} from "aws-cdk-lib";
 import { Construct } from "constructs";
 const appBucket = require("./appBucket");
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export interface AppStackProps extends cdk.StackProps {}
+export interface AppStackProps extends StackProps {}
 
-export class AppStack extends cdk.Stack {
+export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: AppStackProps) {
     super(scope, id, props);
     const frontendBucket = appBucket(this, "FrontendBucket");
