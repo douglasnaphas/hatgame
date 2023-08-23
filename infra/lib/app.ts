3cdde2ab4aa53350961fe36ba1c89811906c4395
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-23 09:12:35 -0400

    Put the frontend behind a distro
    gh-6

--- infra/lib/app.ts
+++ infra/lib/app.ts
@@ -1,13 +1,51 @@
-import {Stack, StackProps, CfnOutput} from "aws-cdk-lib";
+import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
 const appBucket = require("./appBucket");
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export interface AppStackProps extends StackProps {}
+export interface AppStackProps extends StackProps {
+  domainName?: string;
+  zoneId?: string;
+}
 
 export class AppStack extends Stack {
-  constructor(scope: Construct, id: string, props?: AppStackProps) {
+  constructor(scope: Construct, id: string, props: AppStackProps) {
     super(scope, id, props);
+    const { domainName, zoneId } = props;
     const frontendBucket = appBucket(this, "FrontendBucket");
+    let hostedZone, wwwDomainName, certificate, domainNames;
+    if (domainName && zoneId) {
+      hostedZone = route53.HostedZone.fromHostedZoneAttributes(
+        this,
+        "HostedZone",
+        { hostedZoneId: zoneId, zoneName: domainName + "." }
+      );
+      wwwDomainName = "www." + domainName;
+      certificate = new acm.Certificate(this, "Certificate", {
+        domainName,
+        subjectAlternativeNames: [wwwDomainName],
+        validation: acm.CertificateValidation.fromDns(hostedZone),
+      });
+      domainNames = [domainName, wwwDomainName];
+    }
+    const distroProps: any = {
+      logBucket: appBucket(this, "DistroLoggingBucket"),
+      logFilePrefix: "distribution-access-logs/",
+      logIncludesCookies: true,
+      defaultBehavior: {
+        origin: new origins.S3Origin(frontendBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+      },
+      defaultRootObject: "index.html",
+      domainNames,
+      certificate,
+    };
+
+    const distro = new cloudfront.Distribution(this, "Distro", distroProps);
   }
 }
