c3954ddc6367a7283cc7b5ae6c2b7325ef761ba6
Douglas Naphas <douglasnaphas@gmail.com>
2023-10-01 22:10:44 -0400

    Try to add a backend API
    gh-14

--- infra/lib/app.ts
+++ infra/lib/app.ts
@@ -1,7 +1,9 @@
-import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
+import { Stack, StackProps, CfnOutput, Duration } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { aws_apigateway as apigw } from "aws-cdk-lib";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import { aws_lambda as lambda } from "aws-cdk-lib";
 import * as route53 from "aws-cdk-lib/aws-route53";
 import { aws_route53_targets as targets } from "aws-cdk-lib";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
@@ -9,7 +11,6 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 // const appBucket = require("./appBucket");
 import { appBucket } from "./appBucket";
 import { appDistro } from "./appDistro";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export interface AppStackProps extends StackProps {
   domainName?: string;
@@ -36,6 +37,19 @@ export class AppStack extends Stack {
       });
       domainNames = [domainName, wwwDomainName];
     }
+    const webHandler = new lambda.Function(this, "WebHandler", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("../lambda-express-hbs"),
+      memorySize: 5000,
+      environment: {
+        NODE_ENV: "production",
+      },
+      timeout: Duration.seconds(20),
+    });
+    const webApi = new apigw.LambdaRestApi(this, "WebApi", {
+      handler: webHandler,
+    });
     const distro = appDistro(this, frontendBucket, domainNames, certificate);
     if (domainName && wwwDomainName && hostedZone) {
       // point the domain name with an alias record to the distro
@@ -63,5 +77,9 @@ export class AppStack extends Stack {
       cfnAliasWWWRecordSet.weight = DNS_WEIGHT;
       cfnAliasWWWRecordSet.setIdentifier = "www-cf-alias";
     }
+
+    // distro.addBehavior({
+
+    // })
   }
 }
