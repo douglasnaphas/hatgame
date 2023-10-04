f554643abbab90bb14044d6849766d4859e6bb71
Douglas Naphas <douglasnaphas@gmail.com>
2023-10-04 19:30:52 -0400

    Generate a room code, don't send the generator

--- lambda-express-hbs/routes/room-code.js
+++ lambda-express-hbs/routes/room-code.js
@@ -3,10 +3,10 @@ var router = express.Router();
 const randomCapGenerator = require("../lib/randomCapGenerator");
 
 router.post("/", function (req, res, next) {
-  const roomCode = randomCapGenerator({ letters: 6 });
+  const roomCodeSequence = randomCapGenerator({ letters: 6 });
   return res.render("your-room-code", {
     title: "Hat Game: Your Room Code",
-    roomCode,
+    roomCode: roomCodeSequence.next().value,
   });
 });
 
