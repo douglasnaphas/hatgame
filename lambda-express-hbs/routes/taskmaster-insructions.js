278fa738387c0a49ba48fc9fdb47f7b8105b71fc
Douglas Naphas <douglasnaphas@gmail.com>
2023-10-04 18:52:02 -0400

    Don't route to t~r-i~ns/t~r-i~ns
    Route to /prod/taskmaster-instructions, not
    prod/taskmaster-instructions/taskmaster-instructions

--- lambda-express-hbs/routes/taskmaster-insructions.js
+++ lambda-express-hbs/routes/taskmaster-insructions.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/taskmaster-instructions", function (req, res, next) {
+router.get("/", function (req, res, next) {
   return res.render("taskmaster-instructions", {
     title: "Hat Game: Taskmaster Instructions",
   });
