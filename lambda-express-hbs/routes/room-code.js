c14db3ac82d09964c9849c64e15e0b2e6eb6ea90
Douglas Naphas <douglasnaphas@gmail.com>
2023-10-25 14:21:14 -0400

    Check for required fields on room-code POST

--- lambda-express-hbs/routes/room-code.js
+++ lambda-express-hbs/routes/room-code.js
@@ -4,6 +4,18 @@ const randomCapGenerator = require("../lib/randomCapGenerator");
 
 router.post("/", function (req, res, next) {
   const roomCodeSequence = randomCapGenerator({ letters: 6 });
+  if (
+    !req.body["room-name"] ||
+    !req.body["taskmaster-name"] ||
+    !req.body["game-location"] ||
+    !req.body["time-limit"]
+  ) {
+    console.log("randomCapGenerator: bad body, req:", req);
+    return res.status(400).send();
+  }
+  const ATTEMPTS = 20;
+
+
   return res.render("your-room-code", {
     title: "Hat Game: Your Room Code",
     roomCode: roomCodeSequence.next().value,
