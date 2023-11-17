9456a4a77546b85347659b810a0c6a46408bd826
Douglas Naphas <douglasnaphas@gmail.com>
2023-11-17 09:08:18 -0500

    Try to get rid of the headless: new warnings
    https://github.com/argos-ci/jest-puppeteer#customizing-puppeteer-instance
    
    I keep getting warnings:
    
      Puppeteer old Headless deprecation warning:
        In the near future `headless: true` will default to the new Headless
    mode
        for Chrome instead of the old Headless implementation. For more
        information, please see
    https://developer.chrome.com/articles/new-headless/.
        Consider opting in early by passing `headless: "new"` to
    `puppeteer.launch()`
        If you encounter any bugs, please report them to
    https://github.com/puppeteer/puppeteer/issues/new/choose.

---
+++ itest/jest-puppeteer.config.cjs
@@ -0,0 +1,11 @@
+// jest-puppeteer.config.cjs
+
+/** @type {import('jest-environment-puppeteer').JestPuppeteerConfig} */
+module.exports = {
+  launch: {
+    dumpio: true,
+    headless: process.env.HEADLESS !== "false" ? "new" : false,
+    product: "chrome",
+  },
+  browserContext: "default",
+};
