879205af8c015368c0dd2bd9dc3425969e33331a
Douglas Naphas <douglasnaphas@gmail.com>
2023-11-22 08:44:33 -0500

    Use cdk CLI 2.110.1, required by dependency
    I got an error:
    
    > This CDK CLI is not compatible with the CDK library used by your
    > application. Please upgrade the CLI to the latest version.

--- infra/deploy-app-stack.sh
+++ infra/deploy-app-stack.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 npx cdk bootstrap
 STACKNAME=$(npx @cdk-turnkey/stackname@2.1.0 --suffix app --hash 6)
-npx cdk deploy --require-approval never ${STACKNAME}
+npx cdk@2.110.1 deploy --require-approval never ${STACKNAME}
--- infra/deploy-role-stack.sh
+++ infra/deploy-role-stack.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 STACKNAME=$(npx @cdk-turnkey/stackname@2.1.0 --suffix role --hash 6)
 npx cdk bootstrap
-npx cdk deploy ${STACKNAME}
+npx cdk@2.110.1 deploy ${STACKNAME}
