1c1151836b1b94d73d388cc33ec4bce944ff686b
Douglas Naphas <douglasnaphas@gmail.com>
2023-11-25 09:18:31 -0500

    Use require, not import, in roster handler
    I'm getting the error:
    
    SyntaxError: Cannot use import statement outside a module

--- infra/lib/ws/rosterConnectHandler/index.js
+++ infra/lib/ws/rosterConnectHandler/index.js
@@ -1,5 +1,5 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 exports.handler = async function (event, context) {
