0e5e04ade6ce8ad707ac8184ce4dda3b18114260
Douglas Naphas <douglasnaphas@gmail.com>
2023-11-25 08:45:59 -0500

    Record the roster connection ids
    So that I can send new player names to taskmasters.

--- infra/lib/ws/rosterConnectHandler/index.js
+++ infra/lib/ws/rosterConnectHandler/index.js
@@ -1,7 +1,31 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 exports.handler = async function (event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-  return {
-    statusCode: 200,
-    body: "Connected",
-  };
+  a;
+  try {
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+    const now = new Date();
+    const putCommand = new PutCommand({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        PK: event.queryStringParmaters.roomcode,
+        SK: `roster_connection#${event.requestContext.connectionId}`,
+        timestamp: now.toISOString(),
+      },
+    });
+    const response = await docClient.send(command);
+    console.log("response", response);
+    return {
+      statusCode: 200,
+      body: "Connected",
+    };
+  } catch (error) {
+    console.log("error", error);
+    return {
+      statusCode: 500,
+      body: "Error",
+    };
+  }
 };
